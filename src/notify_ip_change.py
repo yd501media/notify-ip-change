165414a4e262d4c8f29982fb2d60a3cb9879140c
yd501media <yd501media@gmail.com>
2025-03-09 01:06:42 +0900

    formetted

--- src/notify_ip_change.py
+++ src/notify_ip_change.py
@@ -1,7 +1,9 @@
-import requests
 import json
 import os
+
+import requests
 from dotenv import load_dotenv
+
 
 def get_global_ip():
     try:
@@ -12,15 +14,18 @@ def get_global_ip():
         print(f"Error retrieving IP address: {e}")
         return None
 
+
 def read_old_ip(file_path):
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
             return file.read().strip()
     return None
 
+
 def write_new_ip(file_path, ip):
     with open(file_path, "w") as file:
         file.write(ip)
+
 
 def send_to_discord(webhook_url, message):
     data = {"content": message}
@@ -32,29 +37,34 @@ def send_to_discord(webhook_url, message):
     except requests.RequestException as e:
         print(f"Error sending message to Discord: {e}")
 
+
 if __name__ == "__main__":
-    dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")
+    dotenv_path = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env"
+    )
     load_dotenv(dotenv_path)
-    
+
     DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
     SERVER_PORT = os.getenv("SERVER_PORT")
     NOTIFICATION_MESSAGE = os.getenv("NOTIFICATION_MESSAGE", "New Server IP Detected")
-    
+
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     IP_FILE_PATH = os.path.join(project_root, "server_ip.txt")
-    
+
     if not DISCORD_WEBHOOK_URL:
-        print("Error: Environment variable DISCORD_WEBHOOK_URL must be set in .env file.")
+        print(
+            "Error: Environment variable DISCORD_WEBHOOK_URL must be set in .env file."
+        )
         exit(1)
-    
+
     ip_address = get_global_ip()
     ip_address_old = read_old_ip(IP_FILE_PATH)
-    
+
     if ip_address and ip_address != ip_address_old:
         if SERVER_PORT:
             message = f"{NOTIFICATION_MESSAGE}: {ip_address}:{SERVER_PORT}"
         else:
             message = f"{NOTIFICATION_MESSAGE}: {ip_address}"
-        
+
         send_to_discord(DISCORD_WEBHOOK_URL, message)
         write_new_ip(IP_FILE_PATH, ip_address)
--- src/set_env.py
+++ src/set_env.py
@@ -1,5 +1,6 @@
-import os
 import argparse
+import os
+
 
 def set_env_variable(env_var, value, env_path):
     os.environ[env_var] = value
@@ -7,21 +8,33 @@ def set_env_variable(env_var, value, env_path):
         file.write(f"{env_var}={value}\n")
     print(f"{env_var} saved to environment variable and .env file at {env_path}.")
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Set environment variables for Discord Webhook URL, Port Number, and Notification Message.")
-    parser.add_argument("--webhook", required=True, help="The Discord Webhook URL to set as an environment variable. (Required)")
-    parser.add_argument("--port", help="The port number to set as an environment variable. (Optional)")
-    parser.add_argument("--message", help="The custom notification message to set as an environment variable. (Optional)")
+    parser = argparse.ArgumentParser(
+        description="Set environment variables for Discord Webhook URL, Port Number, and Notification Message."
+    )
+    parser.add_argument(
+        "--webhook",
+        required=True,
+        help="The Discord Webhook URL to set as an environment variable. (Required)",
+    )
+    parser.add_argument(
+        "--port", help="The port number to set as an environment variable. (Optional)"
+    )
+    parser.add_argument(
+        "--message",
+        help="The custom notification message to set as an environment variable. (Optional)",
+    )
     args = parser.parse_args()
-    
+
     # Ensure .env is created in the project root (~/notify_ip_change/.env)
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     env_path = os.path.join(project_root, ".env")
-    
+
     set_env_variable("DISCORD_WEBHOOK_URL", args.webhook, env_path)
-    
+
     if args.port:
         set_env_variable("SERVER_PORT", args.port, env_path)
-    
+
     if args.message:
         set_env_variable("NOTIFICATION_MESSAGE", args.message, env_path)
